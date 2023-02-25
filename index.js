6d1072dd5c7fa9825993b2ea98af2e9223a286b0
MarilynWho <viktoriia.cheche@gmail.com>
2023-02-25 22:37:14 +0000

    Add manager prompt

--- index.js
+++ index.js
@@ -1,16 +1,103 @@
-// const Manager = require("./lib/Manager");
+import { Manager } from "./lib/Manager.js";
 import { Engineer } from "./lib/Engineer.js";
-// const Intern = require("./lib/Intern");
-// const inquirer = require("inquirer");
-// const path = require("path");
-// const fs = require("fs");
+import { Intern } from "./lib/Intern.js";
+import inquirer from "inquirer";
+import path from "path";
+import fs from "fs";
+import * as url from "url";
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// const render = require("./src/page-template.js");
+import render from "./src/page-template.js";
 
+let flag = true;
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const collectAnswers = [];
 
-const Vanya = new Engineer();
+// const engineer1 = new Engineer("Vanya", "1", );
+function start() {
+    inquirer.prompt(questions["Manager"]).then(function (answers) {
+      collectAnswers.push(answers);
+    });
+}
+
+const questions = {
+  Manager: [
+    {
+      type: "input",
+      message: "Enter team manager's name",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Enter team manager's ID",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Enter team manager's email",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter team manager's Office number",
+      name: "officeNumber",
+    },
+  ],
+  Engineer: [
+    {
+      type: "input",
+      message: "Enter Engineer's name",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Enter Engineer's ID",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Enter Engineer's email",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter Engineer's Github username",
+      name: "gitHub",
+    },
+  ],
+  Intern: [
+    {
+      type: "input",
+      message: "Enter intern's name",
+      name: "name",
+    },
+    {
+      type: "input",
+      message: "Enter intern's ID",
+      name: "id",
+    },
+    {
+      type: "input",
+      message: "Enter intern's email",
+      name: "email",
+    },
+    {
+      type: "input",
+      message: "Enter intern's school",
+      name: "school",
+    },
+  ],
+  AddTeamMember: [
+    {
+      type: "list",
+      message: "Add an:",
+      name: "nextMember",
+      choices: ["Engineer", "Intern", "Finish building the team"],
+    },
+  ],
+};
+
+start();

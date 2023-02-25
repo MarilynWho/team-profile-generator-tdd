3dc11311056c8c8d8304153a113fd4122f991c6c
MarilynWho <viktoriia.cheche@gmail.com>
2023-02-25 20:38:25 +0000

    Implemented Enginer class

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,4 @@
-import { Engineer } from "./lib/Engineer.js";
+import { Engineer } from "../lib/Engineer.js";
 
 test("Can set GitHUb account via constructor", () => {
   const testValue = "GitHubUser";
